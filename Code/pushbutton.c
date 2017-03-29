52fc35b80c736e5c084576d733bc160bb1d4aa12
Vikram Manja <vmanja@purdue.edu>
2017-03-28 23:31:05 -0400

    Added working pushbutton recognition

--- Code/pushbutton.c
+++ Code/pushbutton.c
@@ -1,22 +1,24 @@
 #include "pushbutton.h"
+
 void timer4_init(void){
     T4CON = 0x0000; // Stop the timer and clear the control register,
     TMR4 = 0x0000; // Clear the timer register
-    PR4 = 0x0F42; // Load the period register with value 4000, since 4000 ticks = 100 microsecond
+    PR4 = SAMPLING_PERIOD; // Load the period register with value 4000, since 4000 ticks = 100 microsecond
     T4CONSET = 0x8020; // Start the timer
-    // set up the core timer interrupt with a Priority of 3 and zero sub-priority
-    ConfigIntTimer4(T4_INT_ON | T4_INT_PRIOR_3);
+    ConfigIntTimer4(T4_INT_ON | T4_INT_PRIOR_3);// interrupt with a Priority of 3 and zero sub-priority
 }
 
 void __ISR(_TIMER_4_VECTOR, ipl3) _Timer4Handler(void){
-    if(PORTDbits.RD11){
-        mPORTAClearBits(0xF0);
-        // call mode_select
-        // might need a static var to hold state of pushbuttons between ISRs
+
+    // static vars keep their value between interrupts
+    static unsigned char pb_status = 1; // 0  = pushed (or not depending on circuit)
+
+    // Toggle LEDs if status changed
+    if((pb_status != PUSH_BUTTON_1) && PUSH_BUTTON_1){
+        mPORTAToggleBits(0xFF);
     }
-        else{
-            //mPORTAClearBits(0xF0);
-        }
-    mT4ClearIntFlag(); // clear the interrupt flag
     
+    // update button status
+    pb_status = PUSH_BUTTON_1;
+    mT4ClearIntFlag(); // clear the interrupt flag
  }
