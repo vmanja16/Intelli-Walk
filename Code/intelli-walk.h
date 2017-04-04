ac7bdd90d72786dd20619a48d208bc3faabc4213
Vikram Manja <vmanja@purdue.edu>
2017-04-04 13:08:11 -0400

    Adding encoder header for motor encoders

--- Code/intelli-walk.h
+++ Code/intelli-walk.h
@@ -10,6 +10,7 @@
 #include "ultrasonic.h"  /* Ultrasonic functions     */
 #include "isd5116.h"     /* ISD5116 I2C functions    */
 #include "pushbutton.h"  /* PUSHBUTTONS ISR functions*/
+#include "encoder.h"     /* MOTOR_ENCODER functions  */
 /* PRAGMAS*/
 #pragma config FNOSC = PRIPLL, POSCMOD = HS, FPLLIDIV = DIV_2, FPLLMUL = MUL_20, FPBDIV = DIV_2, FPLLODIV = DIV_1
 #pragma config FWDTEN = OFF
@@ -17,6 +18,7 @@
 
 /* GLOBAL VARIABLES*/
 // volatile --> variables used in main && ISR
+volatile UINT8 encoder_count = 1;
 unsigned int Timer1_count = 0; // Number of Timer 1 Interrupts 
 unsigned int Echo1_count = 0;  // Number of Timer1 interrupts in echo1
 unsigned int Echo2_count = 0;  // Number of Timer1 interrupts in echo2
@@ -25,5 +27,6 @@ volatile unsigned char Obstacle_2 = 0;
 volatile op_mode mode = IDLE;
 
 UINT8 status = 0xFF;
+UINT16 address = 0x1234;
 
 #endif /* _INTELLI-WALK_H */
